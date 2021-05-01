af22e48dd9fdb01e3d844e2ebb022f43ef46f0bc
zhangjie2 <zhangjie2@guahao.com>
2021-05-02 07:44:58 +0800

    2-6 开发Hello World接口 访问地址：http://127.0.0.1:8081/hello

--- src/main/java/com/jiawa/wiki/config/WikiApplication.java
+++ src/main/java/com/jiawa/wiki/config/WikiApplication.java
@@ -1,10 +1,12 @@
-package com.jiawa.wiki;
+package com.jiawa.wiki.config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
-
+@ComponentScan("com.jiawa")
+//@ComponentScan({"com.jiawa","com.test"})
 @SpringBootApplication
 public class WikiApplication {
     private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
---
+++ src/main/java/com/jiawa/wiki/controller/testController.java
@@ -0,0 +1,25 @@
+package com.jiawa.wiki.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+public class testController {
+    /**
+     * get，post，put，delete
+     * /user/id=1
+     * /user/1   resful风格写法
+     * @return
+     */
+    //这个接口支持所有的请求方式
+    //    @GetMapping("hello")
+    //    @PostMapping("/hello")
+    //    @PutMapping("/hello")
+    //    @DeleteMapping("/hello")
+    //    @RequestMapping(value = "/hello",method = RequestMethod.GET)  等同于@GetMapping("hello")
+    //    @RequestMapping(value = "/hello",method = RequestMethod.DELETE)   等同于@DeleteMapping("/hello")
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello world";
+    }
+}
