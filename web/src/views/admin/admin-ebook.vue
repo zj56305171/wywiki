ca47fcc4e28655666b650830c31016013235f0b5
zhangjie <357301361@qq.com>
2022-04-28 13:34:28 +0800

    6-7 点击编辑按钮时，弹出对话框

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -16,7 +16,7 @@
         </template>
         <template v-slot:action="{ text, record }">
           <a-space size="small">
-            <a-button type="primary">
+            <a-button type="primary" @click="edit">
               编辑
             </a-button>
             <a-button type="danger" >
@@ -27,6 +27,14 @@
       </a-table>
     </a-layout-content>
   </a-layout>
+  <a-modal
+      title="电子书表单"
+      v-model:visible="modalVisible"
+      :confirm-loading="modalLoading"
+      @Ok="handleModalOk"
+  >
+    <p>test</p>
+  </a-modal>
 </template>
 
 <script lang="ts">
@@ -114,6 +122,21 @@ export default defineComponent({
       });
     };
 
+    // -------- 表单 ---------
+    const modalVisible = ref(false);
+    const modalLoading = ref(false);
+    const handleModalOk = () => {
+      modalLoading.value = true;
+      setTimeout(() => {
+        modalVisible.value = false;
+        modalLoading.value = false;
+      }, 2000);
+    };
+
+    const edit = () => {
+      modalVisible.value = true;
+    };
+
     onMounted(() => {
       handleQuery({
         page:1,
@@ -127,8 +150,11 @@ export default defineComponent({
       pagination,
       columns,
       loading,
-      handleTableChange
-
+      handleTableChange,
+      edit,
+      modalVisible,
+      modalLoading,
+      handleModalOk
     }
   }
 });
