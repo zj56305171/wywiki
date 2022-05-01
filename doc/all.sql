e4d82979721ace3095af437abe0231415111c09d
zhangjie <357301361@qq.com>
2022-05-01 08:40:08 +0800

    7-2 分类表设计及持久层代码生成 基本的无限级树形表结构设计：id+parent

--- doc/all.sql
+++ doc/all.sql
@@ -37,3 +37,28 @@ create table `demo` (
 ) engine=innodb default charset=utf8mb4 comment='测试';
 
 insert into `demo` (id, name) VALUES (1,'测试');
+
+drop table if exists `category`;
+create table `category`(
+    `id` bigint not null comment 'id',
+    `parent` bigint not null default 0 comment '父id',
+    `name` varchar(50) not null comment '名称',
+    `sort` int comment '顺序',
+    primary key (`id`)
+)engine=innodb default charset=utf8mb4 comment='分类';
+
+insert into `category` (id, parent, name,sort) values (100, 000, '前端开发',100);
+insert into `category` (id, parent, name,sort) values (101, 100, 'VUE',101);
+insert into `category` (id, parent, name,sort) values (102, 100, 'HTML&Css',102);
+insert into `category` (id, parent, name,sort) values (200, 000, 'JAVA',200);
+insert into `category` (id, parent, name,sort) values (201, 200, '基础应用',201);
+insert into `category` (id, parent, name,sort) values (202, 200, '框架应用',202);
+insert into `category` (id, parent, name,sort) values (300, 000, 'Python',300);
+insert into `category` (id, parent, name,sort) values (301, 300, '基础应用',301);
+insert into `category` (id, parent, name,sort) values (302, 300, '进阶方向应用',302);
+insert into `category` (id, parent, name,sort) values (400, 000, '数据库',400);
+insert into `category` (id, parent, name,sort) values (401, 400, 'MYSQL',401);
+insert into `category` (id, parent, name,sort) values (500, 000, '其它',500);
+insert into `category` (id, parent, name,sort) values (501, 500, '服务器',501);
+insert into `category` (id, parent, name,sort) values (502, 500, '开发工具',502);
+insert into `category` (id, parent, name,sort) values (503, 500, '热门服务器语言',503);
