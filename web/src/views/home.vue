233d9bbe8468f7b27fd7c019706a2f35afe9667e
zhangjie <357301361@qq.com>
2022-05-12 10:40:14 +0800

    8-12 修复BUG，router-link的to属性增加冒号

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -37,7 +37,9 @@
             </template>
             <a-list-item-meta :description="item.description">
               <template #title>
-                <router-link to="'/doc?ebookId='+ item.id">{{ item.name }}</router-link>
+                <router-link :to="'/doc?ebookId=' + item.id">
+                  {{ item.name }}
+                </router-link>
               </template>
               <template #avatar><a-avatar :src="item.cover" /></template>
             </a-list-item-meta>
