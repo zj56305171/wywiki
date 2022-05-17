d0bf72dab59adb5f94dbc19cec78d8f0af99c6fe
zhangjie <357301361@qq.com>
2022-05-17 12:32:35 +0800

    9-3 完成用户表基本增删改查功能

---
+++ src/main/java/com/jiawa/wiki/service/UserService.java
@@ -0,0 +1,80 @@
+package com.jiawa.wiki.service;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.jiawa.wiki.domain.User;
+import com.jiawa.wiki.domain.UserExample;
+import com.jiawa.wiki.mapper.UserMapper;
+import com.jiawa.wiki.req.UserQueryReq;
+import com.jiawa.wiki.req.UserSaveReq;
+import com.jiawa.wiki.resp.PageResp;
+import com.jiawa.wiki.resp.UserQueryResp;
+import com.jiawa.wiki.util.CopyUtil;
+import com.jiawa.wiki.util.SnowFlake;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(UserService.class);
+
+    @Resource
+    private UserMapper userMapper;
+
+    @Resource
+    private SnowFlake snowFlake;
+
+    public PageResp<UserQueryResp> list(UserQueryReq req){
+        UserExample userExample = new UserExample();
+        UserExample.Criteria criteria = userExample.createCriteria();
+        if(!ObjectUtils.isEmpty(req.getLoginName())) {
+            criteria.andNameLike("%" + req.getLoginName() + "%");
+        }
+        PageHelper.startPage(req.getPage(), req.getSize());
+        List<User> userList = userMapper.selectByExample(userExample);
+
+        PageInfo<User> pageInfo = new PageInfo<>(userList);
+        LOG.info("总行数：{}",pageInfo.getTotal());
+        LOG.info("总页数：{}",pageInfo.getPages());
+
+
+//        List<UserResp> respList = new ArrayList<>();
+//        for (User user : userList) {
+//            UserResp userResp = new UserResp();
+////            userResp.setId(user.getId());
+//        对象复制
+//            BeanUtils.copyProperties(user,userResp);
+//            CopyUtil.copy(user,UserResp.class);
+//            respList.add(userResp);
+//        }
+
+        List<UserQueryResp> list = CopyUtil.copyList(userList, UserQueryResp.class);
+        PageResp<UserQueryResp> pageResp = new PageResp();
+        pageResp.setTotal(pageInfo.getTotal());
+        pageResp.setList(list);
+        return pageResp;
+    }
+
+    public void save(UserSaveReq req){
+        User user = CopyUtil.copy(req,User.class);
+        if(ObjectUtils.isEmpty(req.getId())){
+            //保存
+            user.setId(snowFlake.nextId());
+            userMapper.insert(user);
+        }else {
+            //更新
+            userMapper.updateByPrimaryKey(user);
+        }
+
+    }
+
+    public void delete(long id){
+        userMapper.deleteByPrimaryKey(id);
+    }
+}
