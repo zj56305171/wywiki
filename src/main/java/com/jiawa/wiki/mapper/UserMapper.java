80ea50c9cf81a0ecc2187387402dcc962b2dd0ef
zhangjie <357301361@qq.com>
2022-05-17 10:18:59 +0800

    9-2 用户表设计，并生成持久层代码

---
+++ src/main/java/com/jiawa/wiki/mapper/UserMapper.java
@@ -0,0 +1,31 @@
+package com.jiawa.wiki.mapper;
+
+import com.jiawa.wiki.domain.User;
+import com.jiawa.wiki.domain.UserExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface UserMapper {
+    long countByExample(UserExample example);
+
+    int deleteByExample(UserExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    List<User> selectByExample(UserExample example);
+
+    User selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
