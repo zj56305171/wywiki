a0c5f7bb80a9f2588200f1682f1888d91130ac06
zhangjie <357301361@qq.com>
2022-05-25 17:06:44 +0800

    11-7 后端增加获取统计数值接口

--- src/main/java/com/jiawa/wiki/mapper/EbookSnapshotMapperCust.java
+++ src/main/java/com/jiawa/wiki/mapper/EbookSnapshotMapperCust.java
@@ -1,5 +1,11 @@
 package com.jiawa.wiki.mapper;
 
+import com.jiawa.wiki.resp.StatisticResp;
+
+import java.util.List;
+
 public interface EbookSnapshotMapperCust {
     public void genSnapshot();
+
+    List<StatisticResp> getStatistic();
 }
--- src/main/java/com/jiawa/wiki/service/EbookSnapshotService.java
+++ src/main/java/com/jiawa/wiki/service/EbookSnapshotService.java
@@ -1,9 +1,11 @@
 package com.jiawa.wiki.service;
 
 import com.jiawa.wiki.mapper.EbookSnapshotMapperCust;
+import com.jiawa.wiki.resp.StatisticResp;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 
 @Service
@@ -14,4 +16,15 @@ public class EbookSnapshotService{
     public void genSnapshot(){
         ebookSnapshotMapperCust.genSnapshot();
     }
+
+    /**
+     * 获取首页数值数据：总阅读数，总点赞数，今日阅读数，今日点赞数
+     * @return
+     */
+    public List<StatisticResp> getStatistic(){
+        return ebookSnapshotMapperCust.getStatistic();
+    }
+
+
+
 }
